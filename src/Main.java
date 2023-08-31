d0072b09255b674904743c8efa4320996cf9f386
Expo <crdailey01@gmail.com>
2023-08-30 21:27:55 -0700

    watched videos on what a trie is. come back tto problem

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+class Trie {
+
+    public Trie() {
+
+    }
+
+    public void insert(String word) {
+
+    }
+
+    public boolean search(String word) {
+
+    }
+
+    public boolean startsWith(String prefix) {
+
+    }
+}
